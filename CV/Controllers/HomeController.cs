e6021d63608be2e49ff2b552c8768198bdff2183
Didier <didier.i.m.r@hotmail.com>
2018-09-06 21:51:54 -0600

    Version 1 - Proyectos en progreso

---
+++ CV/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CV.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Studies()
+        {
+            ViewBag.Message = "Estudios llevados.";
+
+            return View();
+        }
+        public ActionResult Proyects()
+        {
+            ViewBag.Message = "Proyectos realizados.";
+
+            return View();
+        }
+
+        public ActionResult Experience()
+        {
+            ViewBag.Message = "Experiencia laboral.";
+
+            return View();
+        }
+    }
+}
